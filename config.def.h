a61190b240a36e75362aec85fc20c3af1ee2c78e
Saúl Valdelvira <saul@saulv.es>
2023-12-30 16:09:32 +0100

    Bind Mod + Shift + Q to kill client

--- config.def.h
+++ config.def.h
@@ -78,7 +78,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
@@ -103,7 +103,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ControlMask,             XK_q,      quit,           {0} },
         // Brightness/Volume Function Keys
         { 0,    XF86XK_MonBrightnessDown,          spawn,       SHCMD("brightnessctl s +5%") },
         { 0,    XF86XK_MonBrightnessUp,            spawn,       SHCMD("brightnessctl s 5%-") },
