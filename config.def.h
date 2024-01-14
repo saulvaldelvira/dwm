8e66758d01c0770c86710e3ee5a110e15dfa8f66
Saúl Valdelvira <saul@saulv.es>
2024-01-14 22:57:22 +0100

    Add keybing Mod + Ctrl + RET to open a tabbed st window

--- config.def.h
+++ config.def.h
@@ -69,6 +69,7 @@ static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ControlMask,           XK_Return, spawn,          {.v = (const char*[]){"tabbed", "-c", "st", "-w", NULL}} },
 /*	{ MODKEY,                       XK_b,      togglebar,      {0} }, */
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
